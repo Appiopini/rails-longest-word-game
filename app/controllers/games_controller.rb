b19641f6516512f6d1c010b6b0c0ebfe2a1406ba
Eduardo Pini <appiopini@gmail.com>
2020-10-08 23:44:00 -0300

    Done Longest

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -11,15 +11,20 @@ class GamesController < ApplicationController
   def score
     @word = params[:word].upcase
     @grid = params[:grid]
+
     valid_in_grid = @word.chars.all? { |letter| @word.count(letter) <= @grid.count(letter) }
+
     url = "https://wagon-dictionary.herokuapp.com/#{@word}"
     response = open(url).read
+
     parsed = JSON.parse(response)
-    valid_in_dict = parsed[found]
+
+    valid_in_dict = parsed["found"]
+
     if valid_in_dict && valid_in_grid
-      @score = @word.size
+      @score = @word.size * 100
     else
-      'you loose'
+      @score = 0
     end
   end
 end
